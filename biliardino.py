12274191babbaa1eb830fcfaef2dde98746c5387
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-12-17 10:34:00 +0100

    Implementata la gestione del vantaggio ed il blocco del cronometro a fine partita con restart in caso di annullamento dell'ultimo goal

--- biliardino.py
+++ biliardino.py
@@ -7,7 +7,7 @@
 
 import tornado.ioloop
 import tornado.web
-from cronometro import Cronometro
+from crono import Crono
 from acmepins import GPIO,PWM
 import time
 import thread
@@ -19,11 +19,24 @@ led_ir_out = PWM("J4.34",38000)
 # Imposta il duty cycle al 50%
 led_ir_out.start(50)
  
-crono=Cronometro()
+crono=Crono()
 blue_score=0
 red_score=0
 last_score="none"
 
+
+#Controllo di chi ha vinto
+def score_check(blue_score,red_score,crono):
+	if abs(blue_score-red_score)>=2 and blue_score>=10:
+		print "blue wins" 
+		crono.stop()
+		return True
+	if abs(blue_score-red_score)>=2 and red_score>=10:
+		print "red wins" 
+		crono.stop()
+		return True
+	return False
+	
 def blink(gpio,times=1,delay=0.5):
 	for i in range(times):
 		gpio.on()
@@ -43,6 +56,7 @@ def game():
 	LED=GPIO("J4.24","LOW")
 
 	while True:
+		
 		if IR_PULL.get_value()==1:
 			print "Pull"
 			blink(LED,1,0.2)
@@ -52,39 +66,46 @@ def game():
 				counter+=1
 				if counter>=2:
 					crono.reset()
+					crono.start()
 					blue_score=0
 					red_score=0
 					last_score="none"
 					print "Reset"
 					break
+					
 			#Annulla l'ultimo goal
 			if last_score=="red" and red_score>0:
 				red_score-=1
 				last_score="none"
+				crono.start()
 			if last_score=="blue" and blue_score>0:
 				blue_score-=1
 				last_score="none"
-			
-		if IR_RED.get_value()==1:
-			blue_score=blue_score+1
-			last_score="blue"
-			print "Goal for blue %d" % blue_score
-			blink(LED,2,0.2)
-			time.sleep(1)
+				crono.start()
 
-		if IR_BLUE.get_value()==1:
-			red_score=red_score+1
-			last_score="red"
-			print "Goal for red %d" % red_score
-			blink(LED,2,0.2)
-			time.sleep(1)
+		if score_check(blue_score,red_score,crono)==False:
+			#Lettura IR porta dei rossi	
+			if IR_RED.get_value()==1:
+				blue_score=blue_score+1
+				last_score="blue"
+				print "Goal for blue %d" % blue_score
+				blink(LED,2,0.2)
+				time.sleep(1)
+
+			#Lettura IR porta dei blue	
+			if IR_BLUE.get_value()==1:
+				red_score=red_score+1
+				last_score="red"
+				print "Goal for red %d" % red_score
+				blink(LED,3,0.2)
+				time.sleep(1)
 
 class get_scores(tornado.web.RequestHandler):
 	def get(self):
 		global crono,blue_score,red_score
 		
 #		print "Remote IP: %s" % repr(self.request.remote_ip)
-		self.write('{"blue_score":"%02d","red_score":"%02d","time_lapse":"%s"}' % (blue_score,red_score,crono.parziale()))
+		self.write('{"blue_score":"%02d","red_score":"%02d","time_lapse":"%s"}' % (blue_score,red_score,crono.get()))
 
 application = tornado.web.Application([
 	(r"/get_scores.json", get_scores),
